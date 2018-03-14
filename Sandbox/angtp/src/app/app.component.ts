2ccbd8ca20def8f18027a2554b17dc9dff39e781
bigbazar <lionel.leclerc44@yahoo.fr>
2018-03-14 16:53:58 +0100

    variable globale locale

--- Sandbox/angtp/src/app/app.component.ts
+++ Sandbox/angtp/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Formation ORSYS';
 }
+
+var mavariableLocale;
+export var mavariableGlobale;
