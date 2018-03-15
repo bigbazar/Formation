3652d7bbfed6898251c6ba6d26463610d880ead4
bigbazar <lionel.leclerc44@yahoo.fr>
2018-03-15 21:24:52 +0100

    npm install angtp

--- Sandbox/angtp/src/app/app.component.ts
+++ Sandbox/angtp/src/app/app.component.ts
@@ -19,25 +19,20 @@ ngOnInit() : void {
   
   
   // Déclaration de l'objet myanimal utilisé dans le HTML
-  animal:Animal=new Carnivore();
+  myanimal:Animal=new Carnivore();
   myzoo:Zoo = new Zoo();
 
   constructor() {
     // Valorisation de la propriété name de l'objet myanimal
     //setInterval(()=>this.myanimal.name=new Date()+'',1000);
-    //setInterval(this.showDate.bind(this),1000);
-    var a:Animal = new Carnivore();
-    this.myzoo.creerAnimal(a);
+    setInterval(this.showDate.bind(this),1000);
   }
 
   showDate(){
-    this.animal.name=new Date+' ';
+    this.myanimal.name=new Date+' ';
   }
 
-  save(){
-    console.log(`save ${this.animal.name}`);
-    this.myzoo.creerAnimal(this.animal);
-  }
+
 }
 
 // 2 Déclarations :
